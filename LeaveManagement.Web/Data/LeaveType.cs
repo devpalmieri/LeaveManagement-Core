fa0c4d41db49c06984c94c08dc67ad2a3818cd58
devpalmieri <dev.palmieri@tim.it>
2023-04-12 12:24:10 +0200

    Add Tables Leave and Migrations

---
+++ LeaveManagement.Web/Data/LeaveType.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public abstract class LeaveType: BaseIdentity
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }    
+    }
+}
