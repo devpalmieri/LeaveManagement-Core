4a3001354b85a4735c3052216af2b50d00fc0404
devpalmieri <dev.palmieri@tim.it>
2024-06-20 07:38:34 +0200

    a

---
+++ LeaveManagement.Application/Configuration/MapperConfig.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using LeaveManagement.Web.Data;
+using LeaveManagement.Web.Models;
+//using SQLitePCL;
+
+namespace LeaveManagement.Web.Configuration
+{
+    public class MapperConfig:Profile
+    {
+        public MapperConfig() { 
+            CreateMap<LeaveType,LeaveTypesVM>().ReverseMap();
+            CreateMap<LeaveTypesVM, LeaveType>().ReverseMap();
+
+            CreateMap<EmployeeListVM, Employee>().ReverseMap();
+            CreateMap<Employee, EmployeeListVM>().ReverseMap();
+
+            CreateMap<Employee, EmployeeAllocationVM>().ReverseMap();
+
+            CreateMap<LeaveAllocation, LeaveAllocationVM>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationEditVM>().ReverseMap();
+
+            CreateMap<LeaveRequest, LeaveRequestCreateVM>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestVM>().ReverseMap();
+        }   
+
+
+    }
+}
